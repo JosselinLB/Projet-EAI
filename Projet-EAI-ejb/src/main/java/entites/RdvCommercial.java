93c188f2aa80f993c3b998477fd83e4134541a51
JosselinLB <josselin.le-boulch3@univ-tlse3.fr>
2021-11-15 09:09:53 +0100

    Classes Affaire, RdvPoseurs et RdvCommercial

---
+++ Projet-EAI-ejb/src/main/java/entites/RdvCommercial.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entites;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Josselin
+ */
+public class RdvCommercial implements Serializable{
+    
+    private int idRdv;
+    private String dateRdv;
+    private String heureRdv;
+    private String idEquipe;
+
+    public int getIdRdv() {
+        return idRdv;
+    }
+
+    public void setIdRdv(int idRdv) {
+        this.idRdv = idRdv;
+    }
+
+    public String getDateRdv() {
+        return dateRdv;
+    }
+
+    public void setDateRdv(String dateRdv) {
+        this.dateRdv = dateRdv;
+    }
+
+    public String getHeureRdv() {
+        return heureRdv;
+    }
+
+    public void setHeureRdv(String heureRdv) {
+        this.heureRdv = heureRdv;
+    }
+
+    public String getIdEquipe() {
+        return idEquipe;
+    }
+
+    public void setIdEquipe(String idEquipe) {
+        this.idEquipe = idEquipe;
+    }
+
+    @Override
+    public String toString() {
+        return "RdvCommercial{" + "idRdv=" + idRdv + ", dateRdv=" + dateRdv + ", heureRdv=" + heureRdv + ", idEquipe=" + idEquipe + '}';
+    }
+    
+    
+    
+}
