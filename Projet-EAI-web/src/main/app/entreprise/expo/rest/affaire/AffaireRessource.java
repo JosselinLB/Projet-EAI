e1a5794a3552f52a90a9f91b0f7d1aab17747850
aulloscar <49066562+aulloscar@users.noreply.github.com>
2021-11-19 11:24:49 +0100

    Commit comme ça mais j'ai pas fait grand chose

---
+++ Projet-EAI-web/src/main/app/entreprise/expo/rest/affaire/AffaireRessource.java
@@ -0,0 +1,30 @@
+
+import javax.ws.rs.core.UriInfo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author OscarAullo
+ */
+public class AffaireRessource {
+    /**
+     * EJB de l'exposition des services de l'affaire.
+     */
+    // A AJOUTER
+    
+    /**
+     * Contexte Web de l'application. Permet de connaitre si besoin son URI.
+     */
+    @Context
+    private UriInfo context;
+    
+    public AffaireRessource(){
+        
+    }
+    
+}
