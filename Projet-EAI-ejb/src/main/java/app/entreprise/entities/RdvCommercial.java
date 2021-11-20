5629945963aa23e379d2defee54a67e948364160
aulloscar <49066562+aulloscar@users.noreply.github.com>
2021-11-20 16:35:00 +0100

    Début d'implémentation

---
+++ Projet-EAI-ejb/src/main/java/app/entreprise/entities/RdvCommercial.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.entreprise.entities;
+
+/**
+ *
+ * @author OscarAullo
+ */
+public class RdvCommercial {
+    public int idRdv;
+    public int idAffaire;
+    public String dateRdv;
+    public String heureRdv;
+    public int idCommercial;
+    
+    /*
+    public rdvCommercial(int idAffaire, int idCommercial){
+        instancier rdvCommercial avec BD
+    }
+    */
+}
+
---
+++ Projet-EAI-ejb/src/main/java/app/entreprise/facade/AffaireFacadeLocal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.entreprise.facade;
+
+import app.entreprise.entities.Affaire;
+
+/**
+ *
+ * @author OscarAullo
+ */
+public interface AffaireFacadeLocal {
+    void create(Affaire affaire);
+    void update(Affaire affaire);
+    Affaire find(Object id);
+    void delete(Affaire affaire);
+    
+}
